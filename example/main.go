60ca485f18fc97510f131bab0b4b960912489a7a
nssy <624029344@qq.com>
2020-08-13 11:24:32 +0800

    modify example, add openldap

--- example/main.go
+++ example/main.go
@@ -5,10 +5,10 @@ import (
 	"github.com/zjmnssy/zlog"
 )
 
-func main() {
-	searchBase := "ou=xx科技,dc=example,dc=com"
-	userName := "test@example.com"
-	password := "123456"
+func ad() {
+	searchBase := "ou=数篷科技,dc=datacloak3,dc=com"
+	userName := "CN=test01,OU=数篷科技,DC=datacloak3,DC=com"
+	password := "start@2018"
 	filter := "(&(SamAccountName=*))"
 	attributes := make([]string, 0)
 	attributes = append(attributes, "SamAccountName")
@@ -45,5 +45,59 @@ func main() {
 		zlog.Prints(zlog.Info, "example", "Sn = %s", entry.GetAttributeValue("sn"))
 		zlog.Prints(zlog.Info, "example", "Givename = %s", entry.GetAttributeValue("givename"))
 	}
+}
 
+func openldap() {
+	searchBase := "dc=zjmdomain,dc=com"
+	userName := "uid=ztest1,dc=zjmdomain,dc=com"
+	password := "qqq111"
+	//filter := "(&(objectClass=inetOrgPerson))"
+	filter := "(&(objectClass=*))"
+	attributes := make([]string, 0)
+	attributes = append(attributes, "cn")
+	attributes = append(attributes, "sn")
+	attributes = append(attributes, "givenName")
+	attributes = append(attributes, "mail")
+	attributes = append(attributes, "entryDN")
+	//attributes = append(attributes, "entryUUID")
+	attributes = append(attributes, "uid")
+	attributes = append(attributes, "mobile")
+
+	c, err := ldap.Connect("10.10.17.166:389", 2000)
+	defer ldap.Close(c)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "example", "connect ad error = %s", err)
+		return
+	}
+
+	_, err = ldap.Login(c, userName, password)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "example", "login ad error = %s", err)
+		return
+	}
+
+	result, err := ldap.Query(c, searchBase, filter, attributes)
+	if err != nil {
+		zlog.Prints(zlog.Warn, "example", "query error = %s", err)
+		return
+	}
+
+	for _, entry := range result.Entries {
+		if entry.GetAttributeValue("cn") == "" {
+			continue
+		}
+
+		zlog.Prints(zlog.Info, "example", "cn = %s", entry.GetAttributeValue("cn"))
+		zlog.Prints(zlog.Info, "example", "sn = %s", entry.GetAttributeValue("sn"))
+		zlog.Prints(zlog.Info, "example", "givenName = %s", entry.GetAttributeValue("givenName"))
+		zlog.Prints(zlog.Info, "example", "mail = %s", entry.GetAttributeValue("mail"))
+		zlog.Prints(zlog.Info, "example", "entryDN = %s", entry.GetAttributeValue("entryDN"))
+		//zlog.Prints(zlog.Info, "example", "entryUUID = %s", entry.GetAttributeValue("entryUUID"))
+		zlog.Prints(zlog.Info, "example", "uid = %s", entry.GetAttributeValue("uid"))
+		zlog.Prints(zlog.Info, "example", "mobile = %s\n", entry.GetAttributeValue("mobile"))
+	}
+}
+
+func main() {
+	openldap()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zjmnssy/ldap
 go 1.13
 
 require (
-	github.com/go-ldap/ldap v0.0.0-20191214221535-a4f79d8a7cda
+	github.com/go-ldap/ldap v0.0.0-20200717144922-63c350fc2713
 	github.com/zjmnssy/zlog v1.0.2
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
 )
